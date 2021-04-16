1621a1d2b846ec182f8a27d1597a55c3a6ccb0fe
Tong Quang Truong <truongwerk@gmail.com>
2021-04-16 12:40:42 +0700

    0.8

--- script/script.js
+++ script/script.js
@@ -1,4 +1,7 @@
 let myLibrary = [];
+if (JSON.parse(window.localStorage.getItem("library")) != null) {
+	myLibrary = JSON.parse(window.localStorage.getItem("library"));
+}
 
 function books(title, author, pages, status) {
 	this.title = title;
@@ -41,7 +44,6 @@ newBookForm.addEventListener("submit", function () {
 });
 
 //Clear old display.
-
 const container = document.querySelector("#container");
 function removeAllChildNodes(parent) {
 	while (parent.firstChild) {
@@ -57,28 +59,75 @@ function displayBooks() {
 		book.id = "book" + i;
 		book.className = "book";
 		container.appendChild(book);
+
 		let title = document.createElement("h1");
 		title.className = "title";
 		title.textContent = `"${myLibrary[i].title}"`;
 		book.appendChild(title);
+
 		let author = document.createElement("h2");
 		author.className = "author";
 		author.textContent = `${myLibrary[i].author}`;
 		book.appendChild(author);
+
 		let pages = document.createElement("h3");
 		pages.className = "pages";
-		pages.textContent = `${myLibrary[i].pages}`;
+		pages.textContent = `${myLibrary[i].pages} pages`;
 		book.appendChild(pages);
-		let status = document.createElement("h4");
-		status.className = "status";
+
+		let status = document.createElement("button");
+		status.dataset.bookIndex = i;
+		status.className = choseClass(myLibrary[i].status);
 		status.textContent = `${myLibrary[i].status}`;
+		status.addEventListener("click", changeStatus);
 		book.appendChild(status);
-		let remove = document.createElement("h4");
-		remove.className = "remove";
+
+		let remove = document.createElement("button");
+		remove.dataset.bookIndex = i;
+		remove.addEventListener("click", removeButton);
+		remove.className = "removeButton";
 		remove.textContent = `Remove`;
 		book.appendChild(remove);
+
+		window.localStorage.setItem("Library", JSON.stringify(myLibrary));
+		console.log(localStorage);
 	}
 }
-let web = new books("Web dev 102", "God", 999, "Not Read");
-myLibrary.push(web);
-displayBooks();
+
+//Status buttons
+function choseClass(status) {
+	if (status == "Not Read") {
+		return "statusNotRead";
+	} else if (status == "Reading") {
+		return "statusReading";
+	} else {
+		return "statusRead";
+	}
+}
+function changeStatus() {
+	let i = this.dataset.bookIndex;
+	switch (myLibrary[i].status) {
+		case "Not Read":
+			myLibrary[i].status = "Reading";
+			displayBooks();
+			break;
+		case "Reading":
+			myLibrary[i].status = "Read";
+			displayBooks();
+			break;
+		case "Read":
+			myLibrary[i].status = "Not Read";
+			displayBooks();
+			break;
+	}
+}
+
+//Remove button
+function removeButton() {
+	myLibrary.splice(this.dataset.bookIndex, 1);
+	displayBooks();
+}
+
+//let web = new books("Web dev 101", "The Odin Project", 100, "Reading");
+//myLibrary.push(web);
+//displayBooks();
