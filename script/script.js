77f9359d5886c56ac1bad17cefa4f75dd925ba21
Tong Quang Truong <truongwerk@gmail.com>
2021-04-17 15:00:12 +0700

    Book

--- script/script.js
+++ script/script.js
@@ -2,15 +2,13 @@ let myLibrary = [];
 if (localStorage.length != 0) {
 	myLibrary = JSON.parse(window.localStorage.getItem("library"));
 }
-function books(title, author, pages, status) {
+function Books(title, author, pages, status) {
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
 	this.status = status;
-	this.info = function () {
-		return `${title} by ${author}, ${pages} pages, ${status}.`;
 	};
-}
+
 
 //Show  and hide form
 const formDiv = document.querySelector("#formDiv");
@@ -32,7 +30,7 @@ function submitNewBook() {
 	const author = document.getElementById("inputAuthor").value;
 	const pages = document.getElementById("inputPages").value;
 	const status = document.getElementById("inputStatus").value;
-	const newBook = new books(title, author, pages, status);
+	const newBook = new Books(title, author, pages, status);
 	myLibrary.push(newBook);
 	document.getElementById("inputTitle").value = null;
 	document.getElementById("inputAuthor").value = null;
