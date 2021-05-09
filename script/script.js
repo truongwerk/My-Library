43e2305e4f53c13ed34d93d0127ce18df1608d02
truongwerk <80037832+truongwerk@users.noreply.github.com>
2021-05-09 17:05:06 +0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -7,10 +7,7 @@ function books(title, author, pages, status) {
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
-	this.status = status;
-	this.info = function () {
-		return `${title} by ${author}, ${pages} pages, ${status}.`;
-	};
+	this.status = status;	
 }
 
 //Show  and hide form
